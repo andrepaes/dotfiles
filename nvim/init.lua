47ab2b67d1be6b1449a93423cb3304c0bff40af7
Andre Luiz <andre.paes@automatiq.com>
2024-05-02 11:32:36 -0300

    new configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,7 @@ Plug("onsails/lspkind-nvim")
 Plug("petertriho/cmp-git")
 Plug("jedrzejboczar/possession.nvim")
 Plug("sainnhe/gruvbox-material")
-Plug("nvim-telescope/telescope-ui-select.nvim")
+Plug('stevearc/dressing.nvim')
 vim.call("plug#end")
 
 local sets = vim.opt
@@ -151,7 +151,6 @@ end
 require("elixir").setup({
 	nextls = {
 		enable = vim.env.ELIXIR_LSP == "nextls",
-		spitfire = true,
 		init_options = {
 			experimental = {
 				completions = {
@@ -202,7 +201,6 @@ require("telescope").setup({
 		},
 	},
 })
-require("telescope").load_extension("ui-select")
 
 vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC<cr>", { noremap = true })
 vim.keymap.set("n", "<c-f>", ":Ag<space>", { noremap = true })
@@ -266,7 +264,7 @@ require("gitsigns").setup({
 		follow_files = true,
 	},
 	attach_to_untracked = true,
-	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -326,10 +324,10 @@ require("gitsigns").setup({
 		-- map("n", "<leader>hu", gs.undo_stage_hunk)
 		-- map("n", "<leader>hR", gs.reset_buffer)
 		map("n", "<leader>hp", gs.preview_hunk)
-		-- map("n", "<leader>hb", function()
-		--   gs.blame_line { full = true }
-		-- end)
-		-- map("n", "<leader>tb", gs.toggle_current_line_blame)
+		map("n", "<leader>hb", function()
+		  gs.blame_line { full = true }
+		end)
+		map("n", "<leader>tb", gs.toggle_current_line_blame)
 		map("n", "<leader>hd", gs.diffthis)
 		map("n", "<leader>hD", function()
 			gs.diffthis("~")
@@ -444,6 +442,8 @@ cmp.setup({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-n>"] = cmp.mapping.complete(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 	}),
@@ -474,4 +474,170 @@ cmp.setup({
 cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline", keyword_length = 2 } }),
+})
+
+require("dressing").setup({
+  input = {
+    -- Set to false to disable the vim.ui.input implementation
+    enabled = true,
+
+    -- Default prompt string
+    default_prompt = "Input",
+
+    -- Trim trailing `:` from prompt
+    trim_prompt = true,
+
+    -- Can be 'left', 'right', or 'center'
+    title_pos = "left",
+
+    -- When true, <Esc> will close the modal
+    insert_only = true,
+
+    -- When true, input will start in insert mode.
+    start_in_insert = true,
+
+    -- These are passed to nvim_open_win
+    border = "rounded",
+    -- 'editor' and 'win' will default to being centered
+    relative = "cursor",
+
+    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    prefer_width = 40,
+    width = nil,
+    -- min_width and max_width can be a list of mixed types.
+    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+    max_width = { 140, 0.9 },
+    min_width = { 20, 0.2 },
+
+    buf_options = {},
+    win_options = {
+      -- Disable line wrapping
+      wrap = false,
+      -- Indicator for when text exceeds window
+      list = true,
+      listchars = "precedes:…,extends:…",
+      -- Increase this for more context when text scrolls off the window
+      sidescrolloff = 0,
+    },
+
+    -- Set to `false` to disable
+    mappings = {
+      n = {
+        ["<Esc>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+      i = {
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+        ["<Up>"] = "HistoryPrev",
+        ["<Down>"] = "HistoryNext",
+      },
+    },
+
+    override = function(conf)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
+  select = {
+    -- Set to false to disable the vim.ui.select implementation
+    enabled = true,
+
+    -- Priority list of preferred vim.select implementations
+    backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+
+    -- Trim trailing `:` from prompt
+    trim_prompt = true,
+
+    -- Options for telescope selector
+    -- These are passed into the telescope picker directly. Can be used like:
+    -- telescope = require('telescope.themes').get_ivy({...})
+    telescope = nil,
+
+    -- Options for fzf selector
+    fzf = {
+      window = {
+        width = 0.5,
+        height = 0.4,
+      },
+    },
+
+    -- Options for fzf-lua
+    fzf_lua = {
+      -- winopts = {
+      --   height = 0.5,
+      --   width = 0.5,
+      -- },
+    },
+
+    -- Options for nui Menu
+    nui = {
+      position = "50%",
+      size = nil,
+      relative = "editor",
+      border = {
+        style = "rounded",
+      },
+      buf_options = {
+        swapfile = false,
+        filetype = "DressingSelect",
+      },
+      win_options = {
+        winblend = 0,
+      },
+      max_width = 80,
+      max_height = 40,
+      min_width = 40,
+      min_height = 10,
+    },
+
+    -- Options for built-in selector
+    builtin = {
+      -- Display numbers for options and set up keymaps
+      show_numbers = true,
+      -- These are passed to nvim_open_win
+      border = "rounded",
+      -- 'editor' and 'win' will default to being centered
+      relative = "editor",
+
+      buf_options = {},
+      win_options = {
+        cursorline = true,
+        cursorlineopt = "both",
+      },
+
+      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- the min_ and max_ options can be a list of mixed types.
+      -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
+      width = nil,
+      max_width = { 140, 0.8 },
+      min_width = { 40, 0.2 },
+      height = nil,
+      max_height = 0.9,
+      min_height = { 10, 0.2 },
+
+      -- Set to `false` to disable
+      mappings = {
+        ["<Esc>"] = "Close",
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+
+      override = function(conf)
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        return conf
+      end,
+    },
+
+    -- Used to override format_item. See :help dressing-format
+    format_item_override = {},
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
 })
